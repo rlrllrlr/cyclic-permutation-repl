d9e7ee355bdc96ee16cbc72e3530916d8cdac442
kurisutoper <kitoper.kurisu@gmail.com>
2014-08-14 11:29:21 -0400

    created genOrdList func in compute.cpp for shorthand

--- compute.cpp
+++ compute.cpp
@@ -10,17 +10,22 @@ void swap(int &alpha, int &beta) {
   alpha = alpha ^ beta;
 }
 
-vector<int> computeCycle(vector<int> given) {
-  //create ordered list (min, min+1, ..., max)
+vector<int> genOrdList(vector<int> given) {
   int min = given.at(0),
       max = given.at(0);
+  vector<int>result;
   vector<int>::iterator git = given.begin();
   for(; git != given.end(); ++git)
     if(*git > max) max = *git;
     else if(*git < min) min = *git;
-  vector<int> result;
   for(; min <= max; ++min)
     result.push_back(min);
+  return result;
+}
+
+vector<int> computeCycle(vector<int> given) {
+  //create ordered list (min, min+1, ..., max)
+  vector<int> result = genOrdList(given);
   //permute ordered list
   /* (a b c ... z) = (a z)(a y)...(a b) */
   vector<int>::reverse_iterator rit = result.rbegin();
